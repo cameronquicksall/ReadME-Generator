50eff799451d2e9f8d65eaea11e3a91b41d73463
Cameron Quicksall <cameron.quicksall70@gmail.com>
2021-12-18 17:24:51 -0600

    added array of questions + function to initialize application

--- index.js
+++ index.js
@@ -1,51 +1,77 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
-const promptUser = () => {
-    return inquirer.prompt([
+// Array of questions for user
+const promptUser = [
     {
         type: 'input',
-        name: 'name',
+        name: 'title',
         message: 'What is your Project Name? (Required)',
-        validate: nameInput => {
-            if (nameInput) {
-            return true;
-        } else {
-            console.log('Please enter your Project Name!');
-            return false;
-            }
-        }
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Provide a description of your project:"
     },
     {
         type: 'input',
-        name: 'Licenses',
-        message: 'Enter your GitHub Username (Required)',
-        validate: description => {
-            if (description) {
-            return true;
-            }
-        }
-    },
-    {
-        type: 'confirm',
-        name: 'confirmAbout',
-        message: 'Would you like to enter some information about yourself for an "About" section?',
-        default: true
+        name: 'installation',
+        message: "Provide the steps for installation:",
     },
     {
         type: 'input',
-        name: 'about',
-        message: 'Provide some information about yourself:',
-        when: ({ confirmAbout }) => confirmAbout
-        }
-    ]);
-};
+        name: 'usage',
+        message: 'Provide some information about your application usage:',
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Provide instructions for testing your application:"
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "Enter your GitHub Username:"
+    },
+    {
+        type: "input",
+        name: "link",
+        message: "Provide the link to your GitHub:"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Provide your email address"
+    },
+    {
+        type: "input",
+        name: "contributors",
+        message: "List all contributing authors",
+    },
+    {
+        type: "checkbox",
+        name: "licenses",
+        choices: [
+            "MIT License",
+            "Mozilla Public License 2.0",
+            "Apache 2.0 License",
+            "Boost Software License 1.0"
+        ]
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+// Function to initialize application and get array of questions to generate in the terminal
+function init() {
+    inquirer.prompt(promptUser)
+    .then(function(data){
+        writeToFile("README-GENERATOR", generateMarkdown(data));
+    })
+}
 
 // Function call to initialize app
 init();
